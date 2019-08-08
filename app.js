60a61bf2931984988b1d62d03aa6170b5bb2b828
dipanshutt1 <deepanshu@tothenew.com>
2019-08-08 16:50:34 +0530

    connection url changed for database

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.use('/graphql',graphqlHTTP({
     schema,
     graphiql:true
 }));
-mongoose.connect('mongodb://localhost:27017/graphql',{useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/graph',{useNewUrlParser:true});
 
+mongoose.connection.once('open',()=>{
+    console.log("application connected to  mongodb");
+});
 app.listen(3001,()=>{
     console.log("the server is started at 3001 port");
 })
